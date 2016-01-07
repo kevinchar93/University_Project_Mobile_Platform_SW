d2813ce20207afdad8c26fd89fba75385c1b7239
Kevin <kevin_ross_charles@live.com>
2016-01-07 17:46:48 +0000

    Define New pins for Lidar and Drive Systems
    ----------
    Define new pins for the Lidar360 and Drive systems classes so that
    these classes can uses these to access named pins

--- main_mob_platform/libraries/Common/PinMap.h
+++ main_mob_platform/libraries/Common/PinMap.h
@@ -13,5 +13,16 @@
 #define COMM_BAUD_RATE 9600
 
 /* Drive System pins */
+#define DS_EN 8   /* Enable pin for the drive system */
+
+#define LEFT_DIR 5   /* Direction pin for left (X) stepper */
+#define LEFT_STEP 2  /* Step pin for left (X) stepper */
+
+#define RIGHT_DIR 6   /* Direction pin for right (Y) stepper */
+#define RIGHT_STEP 3  /* Step pin for right (Y) stepper */
 
 /* Lidar System pins */
+#define LIDAR_DIR 7   /* Direction pin for LIDAR (Z) stepper */
+#define LIDAR_STEP 4  /* Step pin for LIDAR (Z) stepper */
+#define LIDAR_BUTTON_A 25
+#define LIDAR_BUTTON_B 26
