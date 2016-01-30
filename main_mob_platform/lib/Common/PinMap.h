af48e3a6e71ebce5a140444506e0c9d98a09a023
Ross <rossgithub@gmail.com>
2016-01-30 20:17:48 +0000

    WIP DiffDrive - Changes to Pinmap.h ---------- Made changes to PinMap.h to support the creation of the DiffDrive module specified pins for the drive steppers

--- main_mob_platform/lib/Common/PinMap.h
+++ main_mob_platform/lib/Common/PinMap.h
@@ -4,32 +4,32 @@
 */
 
 /* Comm system pins */
-#define COMM_TX_PIN 18          // we transmit from this pin
-#define COMM_RX_PIN 19          // we recieve on this pin
-#define COMM_ENABLE_PIN -1      // not used
-#define COMM_STATE_PIN 22
-#define COMM_BUTTON_PIN 23
-#define COMM_LED_PIN 24
-#define COMM_BAUD_RATE 9600
+#define COMM_TX_PIN         18          /* We transmit from this pin */
+#define COMM_RX_PIN         19          /* We recieve on this pin */
+#define COMM_ENABLE_PIN     -1          /* Not used */
+#define COMM_STATE_PIN      22
+#define COMM_BUTTON_PIN     23
+#define COMM_LED_PIN        24
+#define COMM_BAUD_RATE      9600
 
 /* Drive System pins */
-#define CNC_BOARD_ENABLE_PIN 8   /* Enable pin for the drive system */
+#define LEFT_MOTOR_DIR      10     /* Direction pin for LEFT drive stepper */
+#define LEFT_MOTOR_STEP     9      /* Step pin for LEFT drive stepper */
+#define LEFT_MOTOR_SLEEP    8      /* Enable pin for LEFT drive stepper */
 
-#define LEFT_DIR 5   /* Direction pin for left (X) stepper */
-#define LEFT_STEP 2  /* Step pin for left (X) stepper */
-
-#define RIGHT_DIR 6   /* Direction pin for right (Y) stepper */
-#define RIGHT_STEP 3  /* Step pin for right (Y) stepper */
+#define RIGHT_MOTOR_DIR     7     /* Direction pin for RIGHT drive stepper */
+#define RIGHT_MOTOR_STEP    6     /* Step pin for RIGHT drive stepper */
+#define RIGHT_MOTOR_SLEEP   5     /* Enable PIN for RIGHT drive stepper */
 
 /* Lidar System pins */
-#define LIDAR_DIR 13            /* Direction pin for LIDAR stepper */
-#define LIDAR_STEP 12           /* Step pin for LIDAR stepper */
-#define LIDAR_MOTOR_SLEEP 11    /* Enable PIN for LIDAR stepper */
+#define LIDAR_DIR           13    /* Direction pin for LIDAR stepper */
+#define LIDAR_STEP          12    /* Step pin for LIDAR stepper */
+#define LIDAR_MOTOR_SLEEP   11    /* Enable PIN for LIDAR stepper */
 
-#define LIDAR_BUTTON_A 18       /* Pin for button labeled A on proto board */
-#define LIDAR_BUTTON_B 19       /* Pin for button labeled B on proto board */
+#define LIDAR_BUTTON_A      18    /* Pin for button labeled A on proto board */
+#define LIDAR_BUTTON_B      19    /* Pin for button labeled B on proto board */
 
-#define LIDAR_MODULE_EN 17      /* Pin to power down and up the lidar module */
+#define LIDAR_MODULE_EN     17    /* Pin to power down and up the lidar module */
 
 /* LCD Screen pins */
 #define LCD_RS 48
